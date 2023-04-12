085cf4ccbe073d2b379548f970fa845f9144417b
Sven Zoutman <sven.zoutman@gmail.com>
2023-04-12 23:36:43 +0200

    Backend AF

--- server.js
+++ server.js
@@ -1,6 +1,13 @@
+/*****************************************************
+ * Define some constants and variables
+ ****************************************************/
 const express = require('express');
 const slug = require('slug');
 const arrayify = require('array-back');
+const app = express();
+const festivals = [];
+const users = [];
+const genres = ["Techno", "HardTechno", "Dance" , "Trance", "House", "EDM", "Drum&Bass", "Dubstep", "Hardcore", "Hardstyle", "Pop", "Hiphop", "Urban"];
 
 
 
@@ -11,12 +18,13 @@ const arrayify = require('array-back');
 require('dotenv').config({ path: '.env' });
 const PORT = process.env.PORT || 3000;
  
-const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
+const { MongoClient, ServerApiVersion } = require('mongodb');
 const uri = process.env.DATABASE_URL;
  
-const client = new MongoClient(
- uri, 
- { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 }
+const client = new MongoClient( uri, { 
+  useNewUrlParser: true, 
+  useUnifiedTopology: true, 
+  serverApi: ServerApiVersion.v1 }
 )
  
 client.connect()
@@ -25,17 +33,14 @@ client.connect()
 
 
 
-
 /*****************************************************
- * Define some constants and variables
+ * Define the mongodb collections
  ****************************************************/
 
-const app = express();
-const port = 3000;
-const festivals = [];
-const genres = ["Techno", "HardTechno", "House", "EDM", "Drum&Bass", "Dubstep", "Hardcore", "Hardstyle", "Pop", "Hiphop"];
-const collection = client.db('test').collection('festivals')
-
+const collectionFestivals = client.db('matching-app').collection('festivals')
+const collectionUpcomingFestivals = client.db('matching-app').collection('upcomingfestivals')
+const collectionMijnFestivals = client.db('matching-app').collection('mijnfestivals')
+const collectionUsers = client.db('matching-app').collection('users')
 
 
 
@@ -48,10 +53,15 @@ app.use(express.json());
 app.use(express.urlencoded({extended: true}));
 
 
+
 /*****************************************************
  * Set template engine
  ****************************************************/
 app.set('view engine', 'ejs');
+
+
+
+
 
 
 /*****************************************************
@@ -68,83 +78,137 @@ app.set('view engine', 'ejs');
  ****************************************************/
 
 
-app.get('/',  (req, res) => {
-  const title =  "festivals";
-    collection.find().toArray()
-      .then(results => {
-        console.log(results)
-    res.render('festivallist', { festivals:results, title, genres})
+
+/*****************************************************
+ * All app.get
+ ****************************************************/
+
+// Load in data homepage
+app.get('/festivals/homepage',  (req, res) => {
+  const title =  "Upcoming Festivals";
+        collectionUpcomingFestivals.find().toArray()
+        .then(resultsUpcoming => {
+          console.log(resultsUpcoming)
+    res.render('homepage', { title, genres, festivals:resultsUpcoming})
     })
     .catch(error => console.error(error))
 });
 
 
-app.get('/festivals/festivallist',  (req, res) => {
-  
-  collection.find({}).toArray().then((festivals) => {
-    res.locals.title = "Festivals";
-    res.render("festivallist", { festivals });
-   });
-
+// Load in data mijn festivals
+app.get('/festivals/mylist',  (req, res) => {
+  const title =  "festivals";
+    collectionMijnFestivals.find().toArray()
+      .then(results => {
+        console.log(results)
+    res.render('myfestivals', { festivals:results, title, genres})
+    })
+    .catch(error => console.error(error))
 });
 
 
-// app.post('/festivals/festivallist',  (req, res) => {
-//   const year = req.body.year;
-//   collection.find({ year }).toArray().then((festivals) => {
-//     res.locals.title = req.body.year;
-//     res.render("festivallist", { festivals, genres });
-//    });
-
-// });
-
-app.post('/festivals/festivallist',  (req, res) => {
-  const genres = req.body.genres;
-  collection.find({ genres }).toArray({}).then((festivals) => {
-    res.locals.title = req.body.genres;
-    res.render("festivallist", { festivals, genres });
-   });
-
+// Load in data top festivals
+app.get('/festivals/topfestivals',  (req, res) => {
+  const title =  "festivals";
+    collectionFestivals.find().toArray()
+      .then(results => {
+        console.log(results)
+    res.render('topfestivals', { festivals:results, title, genres})
+    })
+    .catch(error => console.error(error))
 });
 
 
+// Load in data account
+app.get('/festivals/account',  (req, res) => {
+  const title =  "Account";
+    collectionUsers.find().toArray()
+      .then(results => {
+        console.log(results)
+    res.render('account', { users:results, title, genres})
+    })
+    .catch(error => console.error(error))
+});
 
 
-// app.get('/festivals/:festivalId', (req, res) => {
-
-//     // FIND festival
-//     const id = req.params.festivalId;
-//     const festival = festivals.find( element => element.id == id);
-//     console.log(festival);
-
-//     // RENDER PAGE
-//     res.render('festivaldetails', {title: `${festival.name}`, festival});
-// });
+// Load in edituser form
+app.get('/festivals/edituser', (req, res) => {
+  res.render('edituser', {title: "Gebruiker Bewerken", genres});
+});
 
 
+// Load in add festival form
 app.get('/festivals/add', (req, res) => {
-
-    res.render('addfestival', {title: "Add a festival", genres});
+  res.render('addfestival', {title: "Add a festival", genres});
 });
 
 
-app.post('/festivals/add', async (req, res) => {
 
-  let festival = {
-    slug: slug(req.body.name),
+/*****************************************************
+ * All app.post
+ ****************************************************/
+
+// Filter by month on homepage
+app.post('/festivals/homepage',  (req, res) => {
+  const month = req.body.month;
+  collectionUpcomingFestivals.find({ month }).toArray().then((festivals) => {
+    res.locals.title = req.body.month;
+    res.render("homepage", { festivals, genres });
+   });
+   
+});
+
+// Filter by genres on topfestivals
+app.post('/festivals/topfestivals',  (req, res) => {
+  const genres = req.body.genres;
+  collectionFestivals.find({ genres }).toArray().then((festivals) => {
+    res.locals.title = req.body.genre;
+    res.render("topfestivals", { festivals, genres });
+   });
+});
+
+
+// Update user
+app.post('/festivals/edituser', async (req, res) => {
+  let user = {
     name: req.body.name, 
     year: req.body.year, 
     genres: arrayify(req.body.genres), 
-};
-  
-  const collection = client.db('test').collection('festivals')
+  };
 
-    await collection.insertOne(req.body)
+await collectionUsers.findOneAndUpdate({}, {
+  $set: {
+      name: req.body.name,
+      year: req.body.year,
+      genres: arrayify(req.body.genres) }
+})
+
+  console.log("Adding user: ", user);
+  // ADD user 
+  users.push(user);
+
+  // RENDER PAGE
+const title =  "Succesfully edited the user";
+  res.render('account', {title, users})
+});
+
+
+// Push new festival to the database
+app.post('/festivals/add', async (req, res) => {
+  let festival = {
+    slug: slug(req.body.name),
+    img: req.body.img,
+    name: req.body.name,
+    land: req.body.land,
+    plaats:req.body.plaats, 
+    year: req.body.year, 
+    genres: arrayify(req.body.genres), 
+};
+    await collectionMijnFestivals.insertOne(req.body)
     .then(result => {
       console.log(result)
     })
     .catch(error => console.error(error))
-
 
     console.log("Adding festival: ", festival);
     // ADD festival 
@@ -152,65 +216,8 @@ app.post('/festivals/add', async (req, res) => {
 
     // RENDER PAGE
   const title =  "Succesfully added the festival";
-  res.render('festivallist', {title, festivals})
+  res.render('myfestivals', {title, festivals})
 });
-
-
-
-app.post("/delete", function (req, res) {
-  client.connect((err) => {
-    if (err) throw err;
-    let query = {
-      name: req.body.name,
-      address: req.body.address ? req.body.address : null,
-      telephone: req.body.telephone ? req.body.telephone : null,
-      note: req.body.note ? req.body.note : null,
-    };
-    client
-      .db("test")
-      .collection("festivals")
-      .deleteOne(query, function (err, obj) {
-        if (err) throw err;
-        console.log("1 document deleted");
-        res.send(`Customer ${req.body.name} deleted`);
-      });
-  });
-});
-
-
-
-// app.get('/festivals/edit', (req, res) => {
-
-//     res.render('editfestival', {title: "EDIT ", genres});
-//   });
-
-
-
-// app.post('/festivals/edit', async (req, res) => {
-//   const {
-//         slug,
-//         name, 
-//         year, 
-//         genres } = req.body
-
-//     console.log("Adding festival: ", festivals);
-//     // ADD festival 
-//     festivals.push(festivals);
-
-//     const collection = client.db('test').collection('festivals')
-
-//     await collection.findOneAndUpdate({}, {
-//         $set: {
-//             slug: slug,
-//             name: name,
-//             year: year,
-//             genres: genres }
-//     })
-
-//     // RENDER PAGE
-//     const title =  "Succesfully added the festival";
-//     res.redirect('festivallist')
-// });
 
 
 
@@ -220,7 +227,7 @@ app.post("/delete", function (req, res) {
  ****************************************************/
 
 app.use(function (req, res) {
-    console.error("Error 404: page nog found");
+  console.error("Error 404: page nog found");
     res.status(404).render('404', {title: "Error 404: page not found"});
 });
 
@@ -229,8 +236,8 @@ app.use(function (req, res) {
  * Start webserver
  ****************************************************/
 
-app.listen(port, () => {
+app.listen(PORT, () => {
   console.log('==================================================\n\n')
-  console.log(`Webserver running on http://localhost:${port}\n\n`);
+  console.log(`Webserver running on http://localhost:${PORT}\n\n`);
   console.log('==================================================\n\n')
 });
